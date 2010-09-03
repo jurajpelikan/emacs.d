8cb971a8da2927f3490cfc4ac76c6aeba639fdb0
Juraj Pelikan <juraj.pelikan@gmail.com>
2010-09-03 15:44:32 +0100

    Fixed toggling of zencoding in mumamo. Added basic rml support.

--- load_on_start/30_html.el
+++ load_on_start/30_html.el
@@ -1,8 +1,12 @@
 (require 'zencoding-mode)
-(add-hook 'sgml-mode-hook 'zencoding-mode)
-
+(add-hook 'sgml-mode-hook 
+          (lambda () 
+            (zencoding-mode t)))
 (load (concat emacsd-path "lisp/nxhtml/autostart.el"))
 
 (add-to-list 'auto-mode-alist
 			 '("\\.html\\'" . django-html-mumamo-mode))
+
+(add-to-list 'auto-mode-alist
+			 '("\\.rml\\'" . django-html-mumamo-mode))
 
