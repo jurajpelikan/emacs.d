4751dabe4ec6939ad3a7ffd7851ad320c4adea46
Juraj Pelikan <juraj.pelikan@gmail.com>
2016-06-02 13:37:26 +0200

    Add javascript customizations

---
+++ load/21-javascript.el
@@ -0,0 +1,9 @@
+;; Javascript
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
+
+;; switch case indetantation
+(setq js-switch-indent-offset 4)
+
+;; 4 spaces html identation
+(setq sgml-basic-offset 4)
